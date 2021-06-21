86a9d35aa5e8aafe33eff9963563d95e93a2bdf8
Rashi Singh <rashisingh3004@gmail.com>
2021-06-21 14:54:39 +0530

    added reverse-array

---
+++ Arrays/reverse_array_1.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+//program to reverse an array or string
+//input = [1,2,3,4]
+//output = [4,3,2,1]
+
+//method 1 - Using one for loop
+//Complexity: (time, auxillary space) - O(n); O(1)
+
+vector<int> rev(vector<int> arr, int n)
+{
+  int temp;
+  for (int i = 0; i < n / 2; i++)
+  {
+    temp = arr[i];
+    arr[i] = arr[n - i - 1];
+    arr[n - i - 1] = temp;
+  }
+  return arr;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  vector<int> arr;
+  int n, ip;
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    while (cin >> ip)
+    {
+      arr.push_back(ip);
+    }
+  }
+  vector<int> op = rev(arr, n);
+  for (int i = 0; i < n; i++)
+  {
+    cout << op[i] << " ";
+  }
+  return 0;
+}
